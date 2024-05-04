84caab376975d05f51909f8984de8337fff1fddf
Akash <aakashsoni8756@gmail.com>
2024-05-04 21:34:04 +0530

    For loop hetrogeneous element

---
+++ ForEachDemo2.java
@@ -0,0 +1,13 @@
+public class ForEachDemo2
+{
+   public static void main(String[] args) 
+   {
+	  Object []values = {12,34.89,'A',true,"NIT"};
+	  	  
+	  
+	  for(Object value : values)
+	  {
+		  System.out.println(value);
+	  }
+   }
+}
