7e7fb74d268cb572d4e7d30685cbae9949d55b54
Akash <aakashsoni8756@gmail.com>
2024-05-06 23:11:49 +0530

    Printing Even number

---
+++ PrintingEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrintingEven
+{
+   public static void main(String args[])
+  {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter starting number: ");
+      int number1 = sc.nextInt(); 
+
+      System.out.print("Enter ending number: ");
+      int number2 = sc.nextInt(); 
+
+      for(int i = number1; i<number2; i++)
+      {
+	if(i % 2 == 0)
+	System.out.println(i);
+      }
+  }
+}
