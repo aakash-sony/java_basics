363868a3f9a7ff2d4f5640370ea3699731501c6e
Akash <aakashsoni8756@gmail.com>
2024-05-29 21:50:28 +0530

    checking priority of data types

---
+++ labTask/PrintDemo.java
@@ -0,0 +1,9 @@
+public class PrintDemo {
+
+	public static void main(String args[])
+	{
+		PrintNumber.printn(10.2f);
+			
+	}
+
+}
---
+++ labTask/PrintNumber.java
@@ -0,0 +1,25 @@
+public class PrintNumber {
+	public static void printn(int n) {
+		System.out.println("In Integer: "+n);
+	}
+
+	public static void printn(long n) {
+		System.out.println("In Long: "+n);
+	}
+
+	public static void printn(double n) {
+		System.out.println("In Double: "+n);
+	}
+
+	public static void printn(float n) {
+		System.out.println("In float: "+n);
+	}
+
+	public static void printn(char c) {
+		System.out.println("In character: "+c);
+	}
+
+	public static void printn(String str) {
+		System.out.println("In String: "+str);
+	}
+}
