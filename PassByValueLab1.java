11919afdde7d1745da1fcc527f9485f5b4dfda0a
Akash <aakashsoni8756@gmail.com>
2024-06-03 22:00:04 +0530

    Pass By Value Lab1

---
+++ PassByValueLab1.java
@@ -0,0 +1,14 @@
+ public class PassByValueLab1{
+
+	public static void main(String[] args) 
+	{
+		 int num = 4;
+		 modifyNumber(num);
+		 System.out.println(num);   
+
+	}
+	public static void modifyNumber(int num)
+	{
+		num = 8;		
+	}
+}
