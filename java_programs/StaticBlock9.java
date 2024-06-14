7eb7688bd50d40c5d867f7c4768d46f6b50f6c4a
Akash <aakashsoni8756@gmail.com>
2024-06-14 21:17:47 +0530

    Static block execution

---
+++ java_programs/StaticBlock9.java
@@ -0,0 +1,12 @@
+class StaticBlock9 {
+		static int a = 50;
+
+		public static void main(String[] args) {
+			int a = 60;
+
+			a = a;
+
+			System.out.println(a);
+			System.out.println(StaticBlock9.a); 
+		}
+	}
