a01de11d20b5473459b380af4bd560f74ce44843
Akash <aakashsoni8756@gmail.com>
2024-06-26 19:29:15 +0530

    ArrayIndex Out Of Bounds Exception

---
+++ java_programs/ExceptionHandling4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class ExceptionHandling4
+{
+	public static void main(String args[])
+	{
+		int arr[];
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter array size: ");
+		int size = sc.nextInt();
+		
+		arr = new int[size];
+		
+		for(int i = 0; i < size; i++)
+		{
+			System.out.print("Enter "+(i+1)+" Element: ");
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("The Array is: ");
+		for(int i = 0; i < size; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.println();
+		try
+		{
+			System.out.println(arr[6]);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		System.out.println("Main ended..");
+	}
+}
