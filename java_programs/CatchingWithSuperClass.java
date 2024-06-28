a9cbc6ccb21ae70bd3319f86c431215eaf5460f6
Akash <aakashsoni8756@gmail.com>
2024-06-28 21:32:29 +0530

    Print stack Trace method

---
+++ java_programs/CatchingWithSuperClass.java
@@ -0,0 +1,17 @@
+public class CatchingWithSuperClass 
+{
+	public static void main(String[] args) 
+	{
+		
+		try
+		{
+			
+		}
+		catch(Exception e)  //Exception OR Throwable
+		{
+          e.printStackTrace();			
+		}
+
+	}
+
+}
