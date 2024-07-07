54658f6d6d0045bf8b88b86a055dde5483913be0
Akash <aakashsoni8756@gmail.com>
2024-07-07 23:03:22 +0530

    ArrayProgram-01

---
+++ java_programs/logical_prashant/ArrayProgram01.java
@@ -0,0 +1,25 @@
+/* 
+  Que- Given an array of integers, return true if 15 appears as either the first or last element in the array.
+  The array will be length 1 or more.
+*/ 
+public class ArrayProgram01
+{
+	public static void main(String args[])
+	{
+		int arr[] = {2, 3, 12, 23, 15};
+		System.out.println(checkingElement(arr));
+			
+	}
+	static boolean checkingElement(int arr[])
+	{
+		if(arr[0] == 15 || arr[arr.length - 1] == 15)
+			{
+				return true; 
+			}
+			else
+			{
+				return false;
+			}
+	}
+	
+}
