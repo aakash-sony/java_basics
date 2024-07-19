5ceaaa56a01d10a6d9905e43570ab5134e7f4293
Akash <aakashsoni8756@gmail.com>
2024-07-19 19:53:02 +0530

    Lambda expression-5

---
+++ java_programs/LambdaDemo5.java
@@ -0,0 +1,21 @@
+@FunctionalInterface
+interface Vehicle
+{
+	void run();  //[SAM -> Single Abstract Method]
+}
+
+public class LambdaDemo5
+{
+	public static void main(String[] args) 
+	{
+		Vehicle car = ()-> System.out.println("Car is Running");
+		
+		Vehicle bike = ()-> System.out.println("Bike is Running");
+		
+		Vehicle bus = ()-> System.out.println("Bus is Running");
+		
+		car.run();  bike.run();  bus.run();
+
+	}
+
+}
