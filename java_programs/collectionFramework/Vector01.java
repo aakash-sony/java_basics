0ee2ce15ee6c5674a5193dbacc77285640336b91
Akash <aakashsoni8756@gmail.com>
2024-07-25 21:21:38 +0530

    Vector method-1

---
+++ java_programs/collectionFramework/Vector01.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+public class Vector01 {
+
+	public static void main(String[] args) 
+	{
+		Vector obj = new Vector(4,2);
+		obj.addElement(new Integer(3));
+		obj.addElement(new Integer(2));
+		obj.addElement(new Integer(5));
+		System.out.println(obj.elementAt(1));
+	}
+}
