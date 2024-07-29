2624bdb627eceeed84779c660580ef038d556fa3
Akash <aakashsoni8756@gmail.com>
2024-07-29 20:59:18 +0530

    LinkedList-01

---
+++ java_programs/collectionFramework/LinkedListDemo01.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+public class LinkedListDemo01
+{
+ public static void main(String args[])
+ { 
+      List<Object> list=new LinkedList<>();
+	  list.add("Ravi");
+	  list.add("Vijay");
+	  list.add("Ravi");
+	  list.add(null);
+	  list.add(42);
+	  
+	  System.out.println("1st Position Element is :"+list.get(1));
+
+	  //Iterator interface 
+	  
+	   Iterator<Object> itr = list.iterator();
+	   itr.forEachRemaining(System.out::println); //JDK 1.8
+	   
+	  
+	 
+  }
+}
